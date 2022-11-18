4f5d01995751e966bc0dd7217e63f002e0b97f9f
llpingll <94099281+llpingll@users.noreply.github.com>
2022-11-18 22:06:07 +0000

    Hover state added

--- script.js
+++ script.js
@@ -2,23 +2,36 @@
 let pcScore = 0;
 let playerScore = 0;
 
-
-
-// Add eventlistener and execute playround on click
-const btns = document.querySelectorAll(".selection div");
-btns.forEach(btn => btn.addEventListener("click", (e) => {
-    console.log(e.target.id);
-    playRound(e.target.id, getComputerChoice());
-}));
-
 // Get nodes
+const btns = document.querySelectorAll(".selection div");
 const playerScoreNode = document.querySelector(".pScore");
 const pcScoreNode = document.querySelector(".pcScore");
 const roundResult = document.querySelector(".roundResult");
 const resultDetail = document.querySelector(".resultDetail");
 const pWeapon = document.querySelector(".pWeapon");
 const pcWeapon = document.querySelector(".pcWeapon");
+const modalContainer = document.querySelector(".modalContainer");
+const announcement = document.querySelector(".announce");
+const playAgain = document.querySelector(".playAgain");
 
+// Events
+btns.forEach(btn => btn.addEventListener("click", (e) => {
+    // console.log(e.target.id);
+    playRound(e.target.id, getComputerChoice());
+}));
+
+playAgain.addEventListener("click", () => {
+    modalContainer.classList.remove("show");
+    // Reset everything
+    pcScore = 0;
+    playerScore = 0;
+    pcWeapon.textContent = "🎮";
+    pWeapon.textContent = "🎮";
+    roundResult.textContent = "Choose your weapon!";
+    resultDetail.textContent = "First to score 5 points wins!";
+    pcScoreNode.textContent = `Player: ${pcScore}`;
+    playerScoreNode.textContent = `Computer: ${playerScore}`;
+});
 
 
 // Game logic
@@ -31,8 +44,19 @@ function capitalize(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
+function endGame() {
+    if (playerScore === 5) {
+        announcement.textContent = "You Win!";
+        modalContainer.classList.add("show");
+    }
+    else if (pcScore === 5) {
+        announcement.textContent = "You Lose!";
+        modalContainer.classList.add("show");
+    }
+}
+
 function playRound(playerSelection, pcSelection) {
-    // Win
+    // Win condition
     if ((playerSelection === "rock" && pcSelection === "scissors") ||
         (playerSelection === "paper" && pcSelection === "rock") ||
         (playerSelection === "scissors" && pcSelection === "paper")) {
@@ -42,6 +66,7 @@ function playRound(playerSelection, pcSelection) {
         pcWeapon.textContent = document.getElementById(`${pcSelection}`).textContent;
         pWeapon.textContent = document.getElementById(`${playerSelection}`).textContent;
     }
+    // Lose condition
     else if ((playerSelection === "rock" && pcSelection === "paper") ||
         (playerSelection === "paper" && pcSelection === "scissors") ||
         (playerSelection === "scissors" && pcSelection === "rock")) {
@@ -51,12 +76,13 @@ function playRound(playerSelection, pcSelection) {
         pcWeapon.textContent = document.getElementById(`${pcSelection}`).textContent;
         pWeapon.textContent = document.getElementById(`${playerSelection}`).textContent;
     }
+    // Draw
     else {
         roundResult.textContent = "It's a draw";
         resultDetail.textContent = "Try again";
         pcWeapon.textContent = "🎮";
         pWeapon.textContent = "🎮";
     }
+    // Check for end of game
+    endGame();
 }
-
-console.log();
